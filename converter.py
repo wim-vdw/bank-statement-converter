ca8ed895a28c9f584f47089283d905d35c30c5fa
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2022-02-23 17:12:55 +0100

    implement BNP converter

--- converter.py
+++ converter.py
@@ -2,7 +2,7 @@ from helpers.argenta import ArgentaConverter
 from helpers.bnp import BNPConverter
 
 INPUT_FILE_ARGENTA = '/Users/wim/Documents/Boechout/Documenten voor notaris/Uitgaven sinds 01-04-2019/Argenta/Argenta-2022.pdf'
-INPUT_FILE_BNP = '/Users/wim/Documents/Boechout/Documenten voor notaris/Uitgaven sinds 01-04-2019/BNP/2020/BE97230000284249-EX-2020-12.pdf'
+INPUT_FILE_BNP = '/Users/wim/Documents/Boechout/Documenten voor notaris/Uitgaven sinds 01-04-2019/BNP/2019/BE97230000284249-EX-2019-4.pdf'
 
 if __name__ == '__main__':
     argenta = ArgentaConverter(INPUT_FILE_ARGENTA, 1)
